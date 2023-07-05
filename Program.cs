ccbfec923dbec5d9d270f192026585415d80e730
Katerina <dmitrieva4446@mail.ru>
2023-07-05 23:24:06 +0200

    Добавила gitignore

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 2
+// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+    Console.Write($"Большее число {a}");
+else
+    Console.Write($"Большее число {b}");
---
+++ Задача_4/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 4
+// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+    Console.Write($"Максимальное число {a}");
+else if (b > c)
+    Console.Write($"Максимальное число {b}");
+else if (c > a)
+    Console.Write($"Максимальное число {c}");
